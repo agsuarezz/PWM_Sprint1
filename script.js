9b12a8e4e0d4ae6cb1ac11841a692a41bd2e0f71
Álvaro García Suárez <alvaro.garcia125@alu.ulpgc.es>
2026-02-24 11:14:07 +0000

    Restructure of index.html

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ async function xLuIncludeFile() {
 
     let file = el.getAttribute("xlu-include-file");
     
-    // 1. Prioridad: Atributos data- del propio elemento
-    // 2. Segunda opción: Objeto global window.attributes
     const globalAttrs = window.attributes || {};
 
     try {
@@ -13,13 +11,10 @@ async function xLuIncludeFile() {
         if (response.ok) {
             let content = await response.text();
 
-            // Combinar variables globales con las locales del elemento
-            // Buscamos todas las llaves {{key}} posibles
             let allKeys = new Set([...Object.keys(globalAttrs), ...Array.from(el.attributes).map(a => a.name.replace('data-', ''))]);
 
             allKeys.forEach(key => {
                 let regex = new RegExp(`{{${key}}}`, "g");
-                // Si el elemento tiene data-title, usa eso. Si no, usa el globalAttrs.title
                 let value = el.getAttribute(`data-${key}`) || globalAttrs[key] || "";
                 content = content.replace(regex, value);
             });
@@ -30,7 +25,6 @@ async function xLuIncludeFile() {
             
             el.parentNode.replaceChild(newNode, el);
             
-            // Recursión para el siguiente elemento
             await xLuIncludeFile();
         } else {
             el.removeAttribute("xlu-include-file");
