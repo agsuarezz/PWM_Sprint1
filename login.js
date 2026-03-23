2999347c2fe2bd8ff955a091d3d0afaa72f60448
agsuarezz <alvarogs5000@gmail.com>
2026-03-23 15:53:28 +0000

    fix: merge conflict solved

--- login.js
+++ login.js
@@ -33,37 +33,3 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
-
-document.addEventListener('DOMContentLoaded', () => {
-    const loginForm = document.querySelector('.login-form-card form');
-
-    if (loginForm) {
-        loginForm.addEventListener('submit', async (evento) => {
-            evento.preventDefault(); 
-            const usuarioIntroducido = document.getElementById('usuario').value;
-            const passwordIntroducida = document.getElementById('password').value;
-
-            try {
-                const respuesta = await fetch('http://localhost:3000/usuarios');
-                const usuariosDB = await respuesta.json();
-
-                const usuarioValido = usuariosDB.find(user => 
-                    user.email === usuarioIntroducido && user.password === passwordIntroducida
-                );
-
-                if (usuarioValido) {
-                    localStorage.setItem('usuarioActivo', JSON.stringify(usuarioValido));
-                    
-                    alert(`¡Bienvenida de nuevo, ${usuarioValido.nombre}!`);
-                    window.location.href = "user-movements.html"; 
-                } else {
-                    alert("Usuario o contraseña incorrectos. Por favor, revísalos.");
-                }
-
-            } catch (error) {
-                console.error("Error:", error);
-                alert("Error de conexión con el servidor.");
-            }
-        });
-    }
-});
