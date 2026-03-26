680877fbc5e69fa259340cf1ebeffc228442382a
Álvaro García Suárez <alvaro.garcia125@alu.ulpgc.es>
2026-03-26 23:22:46 +0000

    added inmediate loading after login and logging out

--- login.js
+++ login.js
@@ -1,24 +1,29 @@
-
 document.addEventListener('templatesReady', () => { 
+    const sesionActiva = sessionStorage.getItem('usuarioSesion');
+
+    if (sesionActiva) {
+        window.location.href = "user-movements.html";
+        return; 
+    }
+
     const loginForm = document.getElementById('login-form');
 
     if (loginForm) {
         loginForm.addEventListener('submit', (evento) => {
             evento.preventDefault(); 
             
-            const inputLogin = document.getElementById('usuario').value;
+            const inputLogin = document.getElementById('usuario').value.trim().toLowerCase();
             const passwordIntroducida = document.getElementById('password').value;
 
             const datosUsuarios = localStorage.getItem('usuarios');
             const usuariosDB = datosUsuarios ? JSON.parse(datosUsuarios) : [];
-
             
             const usuarioValido = usuariosDB.find(user => 
-                user.email === inputLogin && user.password === passwordIntroducida
+                user.email.trim().toLowerCase() === inputLogin && user.password === passwordIntroducida
             );
 
             if (usuarioValido) {
-                localStorage.setItem('usuarioSesion', JSON.stringify(usuarioValido));
+                sessionStorage.setItem('usuarioSesion', JSON.stringify(usuarioValido));
                 
                 alert(`¡Bienvenido de nuevo, ${usuarioValido.nombre}!`);
                 window.location.href = "user-movements.html"; 
